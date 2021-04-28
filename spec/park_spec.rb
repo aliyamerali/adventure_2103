2f992482777e36cc79135382cf91463f1c8b9426
Aliya Merali <merali4@gmail.com>
2021-04-28 09:09:26 -0600

    Add initialize tests for park

---
+++ spec/park_spec.rb
@@ -0,0 +1,21 @@
+require './lib/park'
+
+RSpec.describe Park do
+
+  describe '#initialize' do
+    park1 = Park.new('Capitol Reef')
+
+    it 'exists' do
+      expect(park).to be_instance_of(Park)
+    end
+
+    it 'has readable name attribute' do
+      expect(park.name).to eq('Capitol Reef')
+    end
+
+    it 'has starts with an empty array of trails' do
+      expect(park.trails).to eq([])
+    end
+  end
+
+end

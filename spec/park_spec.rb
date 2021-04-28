5d5f1a6d04ea40b5464d3232a7487ccc9de3ab28
Aliya Merali <merali4@gmail.com>
2021-04-28 09:14:06 -0600

    Add test for add trail

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -1,4 +1,5 @@
 require './lib/park'
+require './lib/trail'
 
 RSpec.describe Park do
 
@@ -15,6 +16,19 @@ RSpec.describe Park do
 
     it 'has starts with an empty array of trails' do
       expect(park.trails).to eq([])
+    end
+  end
+
+  describe '#add_trail' do
+    park = Park.new('Capitol Reef')
+    trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
+    trail2 = Trail.new({name: 'Cohab Canyon', length: '1.7 miles', level: :moderate})
+
+    it 'adds a trail to the trails array' do
+      park.add_trail(trail1)
+      park.add_trail(trail2)
+
+      expect(park.trails).to eq([trail1, trail2])
     end
   end
 
